55f0806e478264a4c97fa4418570cf990bbae348
tfarinacci <tfarinacci@me.com>
2022-09-26 14:47:32 -0400

    push updates

--- online-boutique-setup.sh
+++ online-boutique-setup.sh
@@ -10,6 +10,10 @@ kubectl create --context="${CLUSTER1}" namespace $NS
 kubectl create --context="${CLUSTER2}" namespace $NS
 read -p "Press any key to continue"
 
+# Enable Istio on namespaces
+kubectl label --context="${CLUSTER1}" namespace $NS istio-injection=enabled --overwrite
+kubectl label --context="${CLUSTER2}" namespace $NS istio-injection=enabled --overwrite
+read -p "Press any key to continue"
 
 # Deploy application
 kubectl apply --context="${CLUSTER1}" -f ./online-boutique/kubernetes-manifest.yaml  -n $NS
@@ -19,15 +23,10 @@ read -p "Press any key to continue"
 kubectl -n online-boutique get pod -w --context=$CLUSTER1
 kubectl -n online-boutique get pod -w --context=$CLUSTER2
 
-# Enable Istio on namespaces
-kubectl label --context="${CLUSTER1}" namespace $NS istio-injection=enabled --overwrite
-kubectl label --context="${CLUSTER2}" namespace $NS istio-injection=enabled --overwrite
-read -p "Press any key to continue"
-
-# Configure Istio
-kubectl apply --context="${CLUSTER1}" -f ./online-boutique/istio-manifest.yaml  -n $NS
-kubectl apply --context="${CLUSTER2}" -f ./online-boutique/istio-manifest.yaml  -n $NS
-read -p "Press any key to continue"
+# # Configure Istio
+# kubectl apply --context="${CLUSTER1}" -f ./online-boutique/istio-manifest.yaml  -n $NS
+# kubectl apply --context="${CLUSTER2}" -f ./online-boutique/istio-manifest.yaml  -n $NS
+# read -p "Press any key to continue"
 
 INGRESS_HOST="$(kubectl --context="${CLUSTER1}" -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')"
 
@@ -35,5 +34,5 @@ echo "$INGRESS_HOST"
 
 curl -v "http://$INGRESS_HOST"
 
-#kubectl -n online-boutique  port-forward deployment/frontend 8080:8080
+#kubectl -n online-boutique --context=$CLUSTER1 port-forward deployment/frontend 8080:8080
 # localhost:8080
