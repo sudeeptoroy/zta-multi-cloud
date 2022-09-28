4c3157b1c3d02b5f040615bb7928cad9fa4f60de
tfarinacci <tfarinacci@me.com>
2022-09-28 07:18:24 -0500

    added demo script

---
+++ run-demo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo -e "########################################################################################"
+echo -e "#   This script will run the the demo of Istio Mutlt-cluster with SPIRE and OPA        #"
+echo -e "########################################################################################"
+read -p "Press any key to begin"
+
+echo -e "########################################################################################"
+echo -e "#   Deploy the replicated architecture                                                 #"
+echo -e "########################################################################################"
+read -p "Press any key to begin"
+
+echo -e "########################################################################################"
+echo -e "#   Deploy the replicated architecture on cluster1                                     #"
+echo -e "########################################################################################"
+read -p "Press any key to begin"
+./6-deploy-replicated.sh cluster1 
+
+echo -e "########################################################################################"
+echo -e "#   Deploy the replicated architecture on cluster2                                     #"
+echo -e "########################################################################################"
+read -p "Press any key to begin"
+./6-deploy-replicated.sh cluster2 
+
+echo -e "########################################################################################"
+echo -e "#   Enable Istio on the replicated architecture on cluster1 and cluster2               #"
+echo -e "########################################################################################"
+read -p "Press any key to begin"
+./7-enable-istio.sh cluster1
+sleep 5
+./7-enable-istio.sh cluster2
+
+echo -e "########################################################################################"
+echo -e "#   Deploy the segregated architecture on cluster1                                     #"
+echo -e "########################################################################################"
+read -p "Press any key to begin"
+./8-deploy-segregated.sh 
+
+echo -e "########################################################################################"
+echo -e "#   Deploy the segregated architecture on cluster3 and disable cluster2                #"
+echo -e "########################################################################################"
+read -p "Press any key to begin"
+./9-deploy-cluster3.sh
+
