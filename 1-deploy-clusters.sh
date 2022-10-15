245c2c9ff81adbd14f48ff6e142a6e8f46376361
sudeepto <sudeepto.roy@gmail.com>
2022-10-15 20:54:07 +0530

    updated files

--- 1-deploy-clusters.sh
+++ 1-deploy-clusters.sh
@@ -21,18 +21,18 @@ eksctl create cluster -f clusters/cluster1.yaml
 echo -e "   Cluster2"
 eksctl create cluster -f clusters/cluster2.yaml
 
-echo -e "   Cluster3"
-eksctl create cluster -f clusters/cluster3.yaml
+#echo -e "   Cluster3"
+#eksctl create cluster -f clusters/cluster3.yaml
 
 
 aws eks update-kubeconfig  --name cluster1
 aws eks update-kubeconfig  --name cluster2
-aws eks update-kubeconfig  --name cluster3
+#aws eks update-kubeconfig  --name cluster3
 
 # Rename Clusters
 kubectx cluster1=tfarinacci@cluster1.us-east-1.eksctl.io
 kubectx cluster2=tfarinacci@cluster2.us-east-1.eksctl.io
-kubectx cluster3=tfarinacci@cluster3.us-east-1.eksctl.io
+#kubectx cluster3=tfarinacci@cluster3.us-east-1.eksctl.io
 
 
 #kubectx cluster1=arn:aws:eks:us-east-1:398573787840:cluster/cluster1
